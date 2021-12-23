565c774d92327097145acf51ab610c90f8060a3c
NickCascella <83091530+NickCascella@users.noreply.github.com>
2021-12-23 15:43:41 -0500

    added 404 page

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //modules
+const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const session = require("express-session");
@@ -31,5 +32,20 @@ app.use(function (req, res, next) {
 
 app.use("/", indexRouter);
 app.use("/home", mainRouter);
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
